f0821f8641014dd5a3dd848862f9d137b6d5d1a6
Aditi Mohanty <rheaditi@users.noreply.github.com>
2025-07-08 16:33:37 +0530

    feat: setup app with TS, SWC, Vite (#1)
    * chore: init preact typescript app
    
    * Revert "chore: init preact typescript app"
    
    This reverts commit cad7d70379591b0401cabba170b9560d8b497da8.
    
    * chore: setup with ts & swc
    
    * chore: basic stuff

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+
+// https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  base: '/rankaholic/',
+  plugins: [react()],
+})
