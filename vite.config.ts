0d610e34eed6ecb9de0a3d4ddd70053d07653afd
Aditi Mohanty <rheaditi@users.noreply.github.com>
2025-07-10 13:11:05 +0530

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  base: '/rankaholic/',
   plugins: [react()],
 })
