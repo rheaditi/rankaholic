ee1fe87efa4f53656314702671d8ca77251f5064
Aditi Mohanty <rheaditi@users.noreply.github.com>
2025-07-10 12:52:24 +0530

    Revert "chore: use docs directory (#2)"
    This reverts commit e5287786575dcd1292fba393786f975a30926ea2.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-/**
- * @see https://vite.dev/config/
- * @type {import('vite').UserConfig}
- */
+
+// https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   base: '/rankaholic/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-  },
 })
